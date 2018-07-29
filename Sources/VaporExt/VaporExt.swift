050147f60118ac9dc789112474ccfeeeb981ac86
Gustavo Perdomo <gperdomor@gmail.com>
2018-07-28 22:55:44 -0400

    configure ci (#1)

--- Sources/VaporExt/VaporExt.swift
+++ Sources/VaporExt/VaporExt.swift
@@ -1,3 +1,11 @@
+//
+//  VaporExt.swift
+//  VaporExt
+//
+//  Created by Gustavo Perdomo on 07/28/18.
+//  Copyright © 2018 Vapor Community. All rights reserved.
+//
+
 struct VaporExt {
     var text = "Hello, World!"
 }
